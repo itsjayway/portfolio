09208ab7e7e0d4aabd4281767295f6385306391b
Jibran Absarulislam (L-H) <jibrana.ja@proton.me>
2025-07-12 20:31:50 -0400

    fix min sizing

--- src/utils/classConstants.jsx
+++ src/utils/classConstants.jsx
@@ -1,24 +1,21 @@
 import { classes } from './classes';
 
-let REDUCED_MOTION_PREFERRED = window.matchMedia(
-  '(prefers-reduced-motion: reduce)'
-).matches;
+let REDUCED_MOTION_PREFERRED = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
-let HOVER_SCALE = REDUCED_MOTION_PREFERRED
-  ? ''
-  : 'hover:scale-[1.02] transition';
+
+let HOVER_SCALE = REDUCED_MOTION_PREFERRED ? '' : 'hover:scale-[1.02] transition';
 let WORK_DESC_SLIDE = REDUCED_MOTION_PREFERRED
   ? ''
   : 'transition-all duration-200 ease-in-out';
 
 const CARD_TITLE =
-  'text-center text-white text-3xl md:text-4xl xl:text-4xl font-bold';
+  'text-center text-white text-2xl md:text-4xl xl:text-4xl font-bold';
 const CARD_ITEM_TITLE =
   'text-white text-3xl md:text-3xl xl:text-4xl whitespace';
 const CARD_ITEM_SUBTITLE =
-  'text-gray-400 text-2xl md:text-2xl xl:text-3xl whitespace-nowrap';
+  'text-gray-400 text-xl md:text-2xl xl:text-3xl whitespace-nowrap';
 const CARD_ITEM_DESCRIPTION =
-  'text-gray-300 text-xl md:text-text-xl xl:text-2xl whitespace-pre-wrap';
+  'text-gray-300 text-xl md:text-xl xl:text-2xl whitespace-pre-wrap';
 
 const DRAWER_BUTTON = classes(
   CARD_ITEM_SUBTITLE,
